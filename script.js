4d2134cd3140fa66381473c84c6d0492cab13c4e
Mateusz <matimatik97@gmail.com>
2020-03-09 21:49:44 +0100

    Main section and correct styles

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const checkOffset = () => {
     const yOff = window.pageYOffset;
     const el = document.querySelector('.to-top-button');
-    yOff > 200 ? el.style.display = "block" : el.style.display = "none";
+    yOff > 200 ? el.style.bottom = "20px" : el.style.bottom = "-50px";
 }
 document.onscroll = () => checkOffset();
 checkOffset();
