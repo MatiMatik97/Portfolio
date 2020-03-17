c2f0c09f5a4fdcceda556259920be6030a359528
Mateusz <matimatik97@gmail.com>
2020-03-17 23:29:12 +0100

    Styles, animations, contact section

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const checkOffset = () => {
     const yOff = window.pageYOffset;
     const el = document.querySelector('.to-top-button');
-    yOff > 200 ? el.style.bottom = "20px" : el.style.bottom = "-50px";
+    yOff > 200 ? el.style.bottom = '20px' : el.style.bottom = '-50px';
 }
 document.onscroll = () => checkOffset();
 checkOffset();
+
+const colors = [
+    {
+        primary: '#071e3d',
+        secondary: '#be9d40',
+        text: '#c0c0c0'
+    },
+    {
+        primary: '#226089',
+        secondary: '#f59042',
+        text: '#e8ffe8'
+    }
+]
+
+let currentColorNumber = 0;
+
+const randomNumber = (min, max) => {
+    const number = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (number !== currentColorNumber) {
+        currentColorNumber = number
+        return number
+    } else {
+        return randomNumber(min, max)
+    }
+}
+
+const changeNavbarColor = () => {
+    const color = colors[randomNumber(0, colors.length - 1)]
+    const root = document.documentElement;
+    root.style.setProperty('--navbar-primary', color.primary);
+    root.style.setProperty('--navbar-secondary', color.secondary);
+    root.style.setProperty('--navbar-text', color.text);
+}
